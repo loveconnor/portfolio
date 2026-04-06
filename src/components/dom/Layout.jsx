19b7692197069a09bb4708ae2ccafba9e2386953
Connor Love <loveconnor2005@gmail.com>
2026-04-06 19:49:39 -0400

    Optimize conditional rendering in Layout component by simplifying the createPortal syntax for the Awwwards badge.

--- src/components/dom/Layout.jsx
+++ src/components/dom/Layout.jsx
@@ -235,8 +235,7 @@ function Layout({ children, layoutRef, mainRef, router }) {
         </ReactTransition>
       </SwitchTransition>
 
-      {isMounted
-        && createPortal(
+      {isMounted && createPortal(
           <div id="awwwards" style={{ position: 'fixed', zIndex: 999, transform: 'translateY(-50%)', top: '50%', left: 0, right: 'auto' }}>
             <a href="https://www.awwwards.com/sites/connor-love" target="_blank" rel="noreferrer">
               <svg width="53.08" height="171.358">
